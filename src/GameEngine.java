83a5fa0cd802addece2bc0319d4ca7bd507606a3
Braxton <byasami6@gmail.com>
2022-03-05 11:44:42 -0800

    Add basic game engine

---
+++ src/GameEngine.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+public abstract class GameEngine extends Thread {
+    private Grid grid;
+    private Timer timer;
+    private String gameDescription;
+
+    // These hold the temporary selections by the user
+    private String firstSelection;
+    private String secondSelection;
+
+    public GameEngine(GameRules rules){
+        grid = new Grid();
+        timer = new Timer(rules.getTotalTime());
+
+        gameDescription = rules.getDescription();
+        firstSelection = null;
+        secondSelection = null;
+    }
+
+    /** This method builds the GUI of the game **/
+    public void buildGame(){}
+
+    /** Verify valid game move **/
+    public boolean verifyMove(String move){
+        return true;
+    }
+
+    /** This method checks if a move can be made, if so, it makes the move and returns true **/
+    public boolean checkAndMakeMove(){
+        return true;
+    }
+
+    /** This method checks if the game has been won, if so, returns true **/
+    public boolean checkGameStatus(){
+        return true;
+    }
+
+    /** This method provides a default runnable game **/
+    public boolean runGame(){
+        String input = "";
+        Scanner sc = new Scanner(System.in);
+        timer.countdown();
+
+        while(timer.getTime() != 0){
+            // Check if there is input
+            if (sc.hasNext() && firstSelection.equals(null)){
+                // User provides input: Potentially (x,y) coordinate on grid of block they select first
+                input = sc.nextLine();
+
+                // Verify valid input
+                if (verifyMove(input)){
+                    firstSelection = input;
+                }
+            }
+            else if (sc.hasNext() && !firstSelection.equals(null)){
+                input = sc.nextLine();
+
+                if (verifyMove(input)){
+                    secondSelection = input;
+                    checkAndMakeMove();
+                    firstSelection = null;
+                    secondSelection = null;
+                }
+            }
+            else if (checkGameStatus()){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void run(){
+        runGame();
+    }
+}
