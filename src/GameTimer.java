57d09ae3a3fe4ce32fa23393eac5d20a215787a2
RicCake <kennyton148@gmail.com>
2022-03-13 16:14:39 -0700

    Update GameTimer.java

--- src/GameTimer.java
+++ src/GameTimer.java
@@ -1,7 +1,12 @@
-import java.util.Timer;
+import javax.swing.Timer;
+
+import java.util.Scanner;
 import java.util.TimerTask;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GameTimer {
+    private Timer test_timer;
     public int time;
 
     public GameTimer(int totalTime){
@@ -18,31 +23,60 @@ public class GameTimer {
         this.time = newTime;
     }
     
+    // /** This method starts the timer and prints the amount of time left **/
+    // public void runTimer(){
+    //     Timer timer = new Timer();
+    //     TimerTask task = new TimerTask()
+    //     {
+    //         @Override
+    //         public void run()
+    //         {
+    //             if(time == 0){
+    //                 // ends the timer and timertask
+    //                 // add in what ever function should be called when timer ends here
+    //                 timer.cancel();
+    //                 timer.purge();
+    //             }
+    //             else
+    //                 System.out.println("Time left:" + time);
+    //                 time -= 1;
+    //         }
+    //     };
+    //     // runs the task each second
+    //     timer.schedule(task, 0, 1000);
+    // }
+
     /** This method starts the timer and prints the amount of time left **/
-    public void runTimer(){
-        Timer timer = new Timer();
-        TimerTask task = new TimerTask()
-        {
+    public void runTimer() {
+        test_timer = new Timer(1000, new ActionListener() {
             @Override
-            public void run()
-            {
-                if(time == 0){
-                    // ends the timer and timertask
-                    // add in what ever function should be called when timer ends here
-                    timer.cancel();
-                    timer.purge();
+            public void actionPerformed(ActionEvent e) {
+                if (time <= 0) {
+                    test_timer.stop();
                 }
-                else
-                    System.out.println("Time left:" + time);
-                    time -= 1;
+                else{
+                    time--;
+                }
             }
-        };
-        // runs the task each second
-        timer.schedule(task, 0, 1000);
+        }
+        );};
+    
+    public void stop(){
+        test_timer.stop();
     }
+    
 
     public static void main(String[] args){
-        GameTimer gameTimer = new GameTimer(10);
+        GameTimer gameTimer = new GameTimer(100);
         gameTimer.runTimer();
+        gameTimer.test_timer.start();
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter username");
+        System.out.println(gameTimer.getTimeRemaining());
+        String userName = myObj.nextLine();  // Read user input
+        System.out.println(gameTimer.getTimeRemaining());
+        if(userName == "stop"){
+            gameTimer.stop();
+        }
     }    
 }
