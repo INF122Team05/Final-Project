cd90b111c2fcc75b22ca0cc8c1a254fdd497b9f6
Kristine Cheng <58013140+kristinecheng@users.noreply.github.com>
2022-03-13 19:34:25 -0700

    Add files via upload

--- src/UserManager.java
+++ src/UserManager.java
@@ -3,58 +3,74 @@ import java.util.*;
 public class UserManager 
 {
     // String username;
-    List<String> users;
+    public List<String> users;
 
     public void userManager() 
     {
         //Type your code
-        users = new ArrayList<String>();
+        this.users = new ArrayList<String>();
     }
 
     // Return boolean status in order to allow later action 
     public boolean login(String name)
     {
         boolean login = false;
-        if (users.contains(name))
+
+        if (this.users == null || this.users.isEmpty())
         {
-            System.out.println("Success to login!!");
+            this.users = new ArrayList<String>();
+            this.users.add(name);
+            System.out.println("New User account created");
             login = true;
         }
-        else
+        else 
         {
-            addUser(name);
-            System.out.println("User account created, please login again.");
+            if (users.contains(name))
+            {
+                // System.out.println("Success to login!!");
+                login = true;
+            }
         }
-
+        
         return login;
     }
 
     public void logout()
     {
         // If we are not saving any data, all data should be removed when logout
-        users.clear();
+        this.users.clear();
 
         System.out.println("System logout success");
     }
 
     public void addUser(String name)
     {
-        if (users.contains(name))
-            System.out.println("User name already exist, please enter another name");
-        else
+        if (users.isEmpty())
+        {
+            // System.out.println("Adding to empty list ");
+            users = new ArrayList<String>();
             users.add(name);
+        }
+        else
+        {
+            if (users.contains(name))
+                System.out.println("User name already exist, please enter another name");
+            else
+                users.add(name);
+        }
+        
     }
 
     public void removeUser(String name)
     {
-        if (users.isEmpty())
+        if (this.users.isEmpty())
             System.out.println("No user inside the system.");
-        else if (!users.contains(name))
+        else if (!this.users.contains(name))
             System.out.println("User name does not exist.");
         else    
         {
             int idx = users.indexOf(name);
-            users.remove(idx);
+            this.users.remove(idx);
         }
     }
 
